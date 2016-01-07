a0332d284818761fc770ae859125bd4ce4e70180
akoskaaa <akos.hochrein@prezi.com>
2016-01-07 23:04:52 +0100

    smarter cals with module exports

---
+++ calc-v2/main.js
@@ -0,0 +1,8 @@
+var calculator = require('./calc.js')
+
+console.log(
+    calculator.add(42, 69),
+    calculator.subtract(69, 42),
+    calculator.multiply(2, 21),
+    calculator.divide(123, 456)
+)
