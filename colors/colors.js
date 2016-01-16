3995537d52458ae06419a79dde614e4958cecaae
akoskaaa <akos.hochrein@prezi.com>
2016-01-16 23:51:31 +0100

    colors exercise

---
+++ colors/colors.js
@@ -0,0 +1,8 @@
+var colors = require('colors');
+
+console.log('First some yellow text'.yellow);
+console.log('Undeline that text'.yellow.underline);
+console.log('Make it red and bold'.red.bold);
+console.log('Double rainbows all day long'.rainbow);
+console.log('drop the bass'.trap);
+console.log('drop the rainbow bass'.trap.rainbow);
